159a8dd226ec934398e87bfcab956aa3a446a9a4
hirobon1690 <hirobon1690@gmail.com>
2022-06-17 21:18:03 +0900

    FIX: Spelling

--- amazon.js
+++ amazon.js
@@ -1,5 +1,5 @@
 // ==UserScript==
-// @name        Amazon URL Shortner
+// @name        Amazon URL Shortener
 // @namespace   https://github.com/hirobon1690/amazon-url-shortner
 // @version     1
 // @include     https://www.amazon.co.jp/*
