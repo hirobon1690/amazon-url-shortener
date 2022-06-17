60e73374c503429ebe4e241b6179836f1085065b
hirobon1690 <hirobon1690@gmail.com>
2022-06-17 13:16:45 +0900

    FIX: metadata

--- amazon.js
+++ amazon.js
@@ -1,9 +1,9 @@
 // ==UserScript==
 // @name        Amazon URL Shortner
-// @namespace   
-// @description AmazonのURLを短縮します。
-// @include     http://*amazon.co.jp/*
-// @exclude     
+// @namespace   https://github.com/hirobon1690/amazon-url-shortner
+// @version     1
+// @include     https://www.amazon.co.jp/*
+// @grant       none
 // ==/UserScript==
 
 history.pushState('','',location.href.match(/\/dp\/.*\//));
