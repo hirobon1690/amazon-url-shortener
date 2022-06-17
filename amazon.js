fadf75d79e7d360305c30b4dfbe153cae7ade059
hirobon1690 <hirobon1690@gmail.com>
2022-06-17 21:21:31 +0900

    UPDATE: Add include for amazon in other regions

--- amazon.js
+++ amazon.js
@@ -3,6 +3,8 @@
 // @namespace   https://github.com/hirobon1690/amazon-url-shortner
 // @version     1
 // @include     https://www.amazon.co.jp/*
+// @include     https://www.amazon.com/*
+// @include     https://www.amazon.co.*
 // @grant       none
 // ==/UserScript==
 
